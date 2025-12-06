4cf192de40388196856a267f2619932406373fef
jeyton-max <junaiko39@gmail.com>
2025-12-06 16:05:21 +0900

    comments#createで@prototype.comments.newを使用

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
-    @comment = Comment.new(comment_params)
+    @comment = @prototype.comments.new(comment_params)
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
